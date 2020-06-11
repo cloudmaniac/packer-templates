31f0d7a4284565232c00423b2ebfcaf2a7b8794a
Romain Decker <deckerromain@gmail.com>
2020-06-11 21:49:39 +0200

    Syntax updated for Packer 1.6 compatibility

--- build-ubuntu-18.04.sh
+++ build-ubuntu-18.04.sh
@@ -2,4 +2,4 @@
 
 echo "Building Ubuntu 18.04.4 LTS (Bionic Beaver) template"
 
-packer build -var-file=var-ubuntu-18.04.json packer-ubuntu.json
+packer build -var-file=var-ubuntu-18.04.json packer-ubuntu-18.04.json
