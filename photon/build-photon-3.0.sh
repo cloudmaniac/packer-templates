6bde1ab3cd6a21c8323195c926fd846ccbf8cc4d
Romain Decker <deckerromain@gmail.com>
2021-03-17 13:16:53 +0100

    Added -force flag

--- photon/build-photon-3.0.sh
+++ photon/build-photon-3.0.sh
@@ -2,4 +2,4 @@
 
 echo "Building Photon OS 3.0 template"
 
-packer build -var-file=../clouds/var-infra-z67.json photon-3.0.json
+packer build -force -var-file=../clouds/var-infra-z67.json photon-3.0.json
--- photon/build-photon-4.0.sh
+++ photon/build-photon-4.0.sh
@@ -2,4 +2,4 @@
 
 echo "Building Photon OS 4.0 template"
 
-packer build -var-file=../clouds/var-infra-z67.json photon-4.0.json
+packer build -force -var-file=../clouds/var-infra-z67.json photon-4.0.json
