7269d57add258138e93e1a3340da79ef75577236
Romain Decker <deckerromain@gmail.com>
2021-03-17 13:15:06 +0100

    Added -force flag

--- ubuntu/build-ubuntu-18.04.sh
+++ ubuntu/build-ubuntu-18.04.sh
@@ -2,4 +2,4 @@
 
 echo "Building Ubuntu 18.04.5 LTS (Bionic Beaver) template"
 
-packer build -var-file=../clouds/var-infra-z57.json ubuntu-18.04.json
+packer build -force -var-file=../clouds/var-infra-z67.json ubuntu-18.04.json
